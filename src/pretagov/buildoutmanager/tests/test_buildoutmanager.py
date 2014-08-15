1e2ac38b67feff06067a0362af39b39521d3c26f
Adam Terrey <adam@pretaweb.com>
2014-08-15 04:25:59 +0000

    added tests

--- src/pretagov/buildoutmanager/tests/test_buildoutmanager.py
+++ src/pretagov/buildoutmanager/tests/test_buildoutmanager.py
@@ -3,6 +3,23 @@
 import unittest
 import doctest
 
+import os
+import tempfile
+import shutil
+
+def setUp(t):
+	t.bm_cwd = os.getcwd()
+	t.bm_wd = tempfile.mkdtemp()
+	os.chdir(t.bm_wd)
+
+def tearDown(t):
+	os.chdir(t.bm_cwd)
+	shutil.rmtree(t.bm_wd)
+	del t.bm_cwd
+	del t.bm_wd
+
+
 def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocFileSuite("test.txt"))
+    tests.addTests(doctest.DocFileSuite("test.txt", globs={'os': os, 'tempfile': tempfile},
+    	setUp=setUp, tearDown=tearDown))
     return tests
