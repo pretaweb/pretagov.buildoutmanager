d6e160cb1e4aaaa8ff0636c2e3221577ab905451
Adam Terrey <adam@pretaweb.com>
2014-08-15 02:51:22 +0000

    created egg

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev0'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='pretaweb.buildoutmanager',
+      version=version,
+      description="Buildout manager for buildout",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Adam Terrey (PretaGov)',
+      author_email='adam@pretagov.com',
+      url='http://www.pretagov.com.au',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['pretaweb'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
